14c6e62a6243f71d8277aaff61081d1b2077576e
Pree Kharb <121867650+OMC-Pree@users.noreply.github.com>
2023-06-02 11:23:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function changeImage() {
-    var imgElement = document.getElementById("image");
-    var randomImageURL = "https://source.unsplash.com/random"; // Replace with your desired API URL
-    imgElement.src = randomImageURL + "?timestamp=" + new Date().getTime();
-}
